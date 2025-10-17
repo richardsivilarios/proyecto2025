09a284828b05ecc1ede7b1abe1ffc89597853b6c
richardsivilarios <richard.sivila.rios@gmail.com>
2025-10-17 16:59:25 -0400

    17-10-2025

---
+++ web/src/views/usuario/index.vue
@@ -0,0 +1,17 @@
+<template>
+<q-layout view="hHh lpR fFf">
+ <q-page-container>
+    <div class="q-pa-md">
+        hola
+
+    </div>
+    
+ </q-page-container>
+</q-layout>
+</template>
+<script setup>
+import {ref,reactive,onMounted} from 'vue'
+
+</script>
+<style lang="sass">
+</style>
