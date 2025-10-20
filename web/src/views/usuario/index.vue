f5c330b2bc8d3e29fe7ee352df4fb2a35c0617ac
richardsivilarios <richard.sivila.rios@gmail.com>
2025-10-20 18:31:47 -0400

    20/10/2025 noche

--- web/src/views/usuario/index.vue
+++ web/src/views/usuario/index.vue
@@ -1,102 +1,74 @@
 <template>
 <q-layout view="hHh lpR fFf">
- <q-page-container>
-    <div class="q-pa-md">
+    <q-page-container>
         <q-table
-        title="Lista de Usuarios"
+        title="Listar Usuarios"
         :columns="columns"
         :rows="state.tableData"
         row-key="idusuario"
-        virtual-scroll
         :visible-columns="visibleColumns"
         >
-        <template v-slot:top-left>
-            <q-btn
-            color="primary"
-            icon="add"
-            label="nuevo cliente"
-            @click="onEdit()"
-            >
-            </q-btn>
-
-        </template> 
+        <template v-slot:top-left >
+        <q-btn
+        color="primary"
+        icon="add"
+        label ="+ usuario"
+        @click = "onEdit()"
+        />
+    </template>
         </q-table>
-
-
-    </div>
- </q-page-container>
+    </q-page-container>
 </q-layout>
 </template>
 <script setup>
-import {ref,reactive,onMounted} from 'vue'
+import {ref, reactive, onMounted} from 'vue'
 import {queryUsuarioTable} from '../../api/usuario/usuario'
-import { errorMsg } from '../../utils/message';
+import {errorMsg} from '../../utils/message'
 import Pagination from '../../components/Pagination.vue'
-
-const state = reactive({
+// columnas llamadas de backend (controller)
+const state = reactive ({
 blurry:'',
 tableData:[],
-userObj:{},
+userObj:[],
 current:1,
-size: 10,
-total:0,
-selectedUsuario: null,
-}
-)
-const columns = [
-    {name:'idusuario',label:'ID Usuario',align:'center',field:'idusuario',sortable:true},
-    {name:'nombre',label:'Nombre',align:'center',field:'nombre',sortable:false},
-    {name:'ap',label:'Apellido \n Paterno',align:'left',field:'ap',sortable:true},
-    {name:'am',label:'Apellido \n Materno',align:'center',field:'am',sortable:true}
-]
-const visibleColumns = ref(['nombre', 'ap'])
-const queryUsuarioTableFun=()=>{
-//loading.value = true 
-const params = {
-    blurry: state.blurry,
-    size: state.size,
-    currentPage: state.current
-}
-queryUsuarioTable(params)
-.then((res)=>{
-//loading.value=false
-if (res.success) {
-    state.tableData = res.data.records
-    state.total = res.data.total
-} else {
-    errorMsg(res.msg)
-}
+size:10,
+total: 0,
 })
-.catch(()=>{
-    //loading.value= false
-    errorMsg('Error al cargar los datos')
-})}  
+const columns = [
+    {name:'idusuario',label:'Id Usuario',align:'center',field:'idusuario',sortable:true},
+    {name:'nombre',label:'Nombre',align:'center',field:'nombre',sortable:true},
+    {name:'ap',label:'Apellido Paterno',align:'left',field:'ap',sortable:true},
+    {name:'am',label:'Apellido Materno',align:'center',field:'am',sortable:true},
+]
+const visibleColumns = ref(['idusuario','nombre'])
+
+// funcion 
+const queryUsuarioTableFun=()=> {
+    const params = {
+        blurry: state.blurry,
+        size:state.size,
+        currentPage: state.current
+    }
+    queryUsuarioTable(params)
+    .then((res)=> {
+            if (res.success) {
+                state.tableData = res.data.records
+                state.total = res.data.total
+            }  else {
+                errorMsg(res.mgs)
+            }
+        }
+    ) .catch(()=>{
+        errorMsg('Error al cargar los datos')
+    } )
+}
 
 const onEdit = () => {
-    console.log('ADICIONAR UN USUARIO')
+    console.log('NUEVO USUARIO')
 }
 onMounted(() => {
 queryUsuarioTableFun()
 })
 </script>
 <style lang="sass">
-.my-sticky-header-column-table
-  height: 80vh
-  width: 100%
-  max-width: 100%
-  background: white
-
-  tr th
-    position: sticky
-    z-index: 2
-    background: #00b4ff
-
-  thead tr:first-child th
-    top: 0
-    z-index: 3
-
-  td:first-child, th:first-child
-    position: sticky
-    left: 0
-    background-color: #e0f7ff
 </style>
